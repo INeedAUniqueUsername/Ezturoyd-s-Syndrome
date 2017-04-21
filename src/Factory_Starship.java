02379fee46e9ca9506e7b05c1fd6dae5826ac06c
INeedAUniqueUsername <alexiscomical445@gmail.com>
2017-04-21 13:46:03 -0700

    Rename

---
+++ src/Factory_Starship.java
@@ -0,0 +1,4 @@
+//Please do not call our establishment a "factory." Unlike "factories," we don't just mass produce bunches of triangles and call them starships; we manufacture premium-quality vehicles for noble travellers of space.
+public class Factory_Starship {
+	private Factory_Starship() {}
+}
