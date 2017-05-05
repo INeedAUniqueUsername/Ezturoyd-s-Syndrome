32289f56467349879ddd980a028805eb43eda8d4
League <cole.trammer@gmail.com>
2017-05-04 17:02:53 -0700

    Starship Exhaust Part I

--- src/Body_StarshipExhaust.java
+++ src/Body_StarshipExhaust.java
@@ -1,10 +1,24 @@
+import java.awt.Graphics;
 
 public class Body_StarshipExhaust extends Body_Projectile {
-
 	public Body_StarshipExhaust(Projectile p) {
 		super(p);
 		// TODO Auto-generated constructor stub
 	}
-	public void updateShapes() {
+
+	public void updateShapes(Graphics g) {
+		SpaceObject owner = getOwner();
+		double pos_r = owner.getPosR();
+		int height = 12;
+		int width = 4;
+		double angle = SpaceObject.arctanDegrees(height, width);
+		double distance = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+		setShapes(createPolygon(
+				owner.polarOffset(pos_r+angle, distance),
+				owner.polarOffset(pos_r-angle, distance),
+				owner.polarOffset(pos_r+angle+180, distance),
+				owner.polarOffset(pos_r-angle+180, distance),
+				owner.polarOffset(pos_r+angle, distance)
+				));
 	}
 }
